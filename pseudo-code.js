49e06b96c683c8fef2824dadc02e4f9bd82aa40f
nwils000 <nwils000@gmail.com>
2024-04-03 10:35:29 -0400

    added some basic pseudocode

---
+++ pseudo-code.js
@@ -0,0 +1,28 @@
+// prettier-ignore
+// MIND READER
+
+    // FUNCTIONALITIES
+
+        // Has 6 pages
+
+        // Each page shares a navigator at the bottom that allows the user to cycle through them
+
+        // The navigator remembers which page you are on and displays it
+
+        // Has the ability to go back home or one page up or one page down
+
+    // HOW TO MAKE THESE FUNCTIONALITIES WORK
+    
+        // Have a central storage for each page held within functions (probably an object)
+
+        // the navigation functions will have 3 functionalities buttons forward backwards or home
+        
+            // Forward function will take **what page were currently on** and increment it by 1
+
+            // Backward function wil take **what page were currently on** and decrement it by 1 
+
+            // Home function will set **the page were currently on** to 1
+        
+        // Each page has a CTA either Go, next, reveal that are buttons that will call the forward function
+        
+        // Each page besides the first will have a home button that will call teh home function
