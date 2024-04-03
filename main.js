71d39d35629965797c71551665baa2c4a940f715
nwils000 <nwils000@gmail.com>
2024-04-03 10:47:39 -0400

    added html structure to the pseudo code portion of main.js

--- main.js
+++ main.js
@@ -1,51 +1,55 @@
 let pages = {
-    pageOne() {
+  pageOne() {},
+  pageTwo() {},
+  pageOne() {},
+  pageOne() {},
+  pageOne() {},
+  pageOne() {},
+};
 
-    },
-    pageTwo() {
+/*
 
-    },
-    pageThree() {
+HTML STRUCTURE
 
-    },
-    pageFour() {
+  <body>
+    <div id="content-container" class="container">
+      <h2 id="heading"></h2>
+      <button class="body-button"></button>
+      <p class="instructions"></p>
+      <button class="footer-button"></button>
+    </div>
+    <script defer src="./pseudo-code.js"></script>
+  </body>
 
-    },
-    pageFive() {
+  
 
-    },
-    pageSix() {
+MIND READER
 
-    },
-}
+    FUNCTIONALITIES
 
-// prettier-ignore
-// MIND READER
+        Has 6 pages
 
-// FUNCTIONALITIES
+            Each page should be a unique function within an object
 
-// Has 6 pages
+        Each page shares a navigator at the bottom that allows the user to cycle through them
 
-// Each page should be a unique function within an object
+        The navigator remembers which page you are on and displays it
 
-// Each page shares a navigator at the bottom that allows the user to cycle through them
+        Has the ability to go back home or one page up or one page down
 
-// The navigator remembers which page you are on and displays it
+    HOW TO MAKE THESE FUNCTIONALITIES WORK
+    
+        Have a central storage for each page held within functions (probably an object)
 
-// Has the ability to go back home or one page up or one page down
+        the navigation functions will have 3 functionalities buttons forward backwards or home
+        
+            Forward function will take **what page were currently on** and increment it by 1
 
-// HOW TO MAKE THESE FUNCTIONALITIES WORK
+            Backward function wil take **what page were currently on** and decrement it by 1 
 
-// Have a central storage for each page held within functions (probably an object)
-
-// the navigation functions will have 3 functionalities buttons forward backwards or home
-
-// Forward function will take **what page were currently on** and increment it by 1
-
-// Backward function wil take **what page were currently on** and decrement it by 1
-
-// Home function will set **the page were currently on** to 1
-
-// Each page has a CTA either Go, next, reveal that are buttons that will call the forward function
-
-// Each page besides the first will have a home button that will call teh home function
+            Home function will set **the page were currently on** to 1
+        
+        Each page has a CTA either Go, next, reveal that are buttons that will call the forward function
+        
+        Each page besides the first will have a home button that will call teh home function
+*/
