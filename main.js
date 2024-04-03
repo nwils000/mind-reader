f80621c04edf8e66c28b81898b27a7ac3d258005
nwils000 <nwils000@gmail.com>
2024-04-03 19:47:08 -0400

    got the first screen to render

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
+const state = { currentPage: 1 };
+
 let icons = {
   pizza: 'url to pizza icon',
   spoon: 'url to spoon icon',
   bowl: 'url to bowl icon',
 };
 
-function loopThroughIcons() {
-  let arrayOfIcons = [];
-  for (const property in icons) {
-    arrayOfIcons.push(icons[property]);
-  }
-  return arrayOfIcons;
-}
-
-let pagesContents = {
+const content = {
   headingsArray: [
     'I can read your mind',
     'Pick a number from 01 - 99',
@@ -41,183 +35,69 @@ let pagesContents = {
   footerButtonsArray: ['Go', 'Home', 'Home', 'Home', 'Home', 'Home'],
 };
 
-let visibleHeading = document.createElement('h2');
-let visibleBodyButton = document.createElement('button');
-let visibleInstructions = document.createElement('p');
-let visibleSubNote = document.createElement('p');
-let footerButton = document.createElement('button');
+function loopThroughIcons() {
+  let arrayOfIcons = [];
+  for (const property in icons) {
+    arrayOfIcons.push(icons[property]);
+  }
+  return arrayOfIcons;
+}
 
-let pages = {
-  template(
-    headingVisible,
-    bodyButtonVisible,
-    instructionsVisible,
-    subNoteVisible,
-    headingContent,
-    bodyButtonContent,
-    instructionsContent,
-    subNoteContent,
-    footerButtonContent,
-    whereBodyButtonGoes,
-    whereFooterButtonGoes
-  ) {
-    if (headingVisible) {
-      visibleHeading.textContent = headingContent;
-      document.body.appendChild(visibleHeading);
-    }
-    if (bodyButtonVisible) {
-      visibleBodyButton.textContent = bodyButtonContent;
-      visibleBodyButton.addEventListener('click', () => {
-        whereBodyButtonGoes();
-      });
-      document.body.appendChild(visibleBodyButton);
-    }
-    if (instructionsVisible) {
-      visibleInstructions.textContent = instructionsContent;
-      document.body.appendChild(visibleInstructions);
-    }
-    if (subNoteVisible) {
-      visibleSubNote.textContent = subNoteContent;
-      document.body.appendChild(visibleSubNote);
-    }
-    footerButton.textContent = footerButtonContent;
-    footerButton.addEventListener('click', () => {
-      whereFooterButtonGoes();
-    });
-    document.body.appendChild(footerButton);
-  },
+function createAndAppendElement(type, parent, content, event) {
+  let element = document.createElement(type);
+  element.textContent = content;
+  parent.appendChild(element);
+}
 
-  pageOne() {
-    pages.template(
-      true,
-      false,
-      false,
-      false,
-      pagesContents.headingsArray[0],
-      pagesContents.bodyButtonsArray[0],
-      pagesContents.instructionsArray[0],
-      pagesContents.subNotesArray[0],
-      pagesContents.footerButtonsArray[0],
-      undefined,
-      pages.pageTwo
-    );
-  },
-  pageTwo() {
-    pages.template(
-      true,
-      true,
-      true,
-      false,
-      pagesContents.headingsArray[1],
-      pagesContents.bodyButtonsArray[1],
-      pagesContents.instructionsArray[1],
-      pagesContents.subNotesArray[1],
-      pagesContents.footerButtonsArray[1],
-      pages.pageThree
-    );
-  },
-  pageThree() {
-    pages.template(
-      true,
-      true,
-      true,
-      true,
-      pagesContents.headingsArray[2],
-      pagesContents.bodyButtonsArray[2],
-      pagesContents.instructionsArray[2],
-      pagesContents.subNotesArray[2],
-      pagesContents.footerButtonsArray[2],
-      pages.pageFour
-    );
-  },
-  pageFour() {
-    pages.template(
-      true,
-      true,
-      true,
-      true,
-      pagesContents.headingsArray[3],
-      pagesContents.bodyButtonsArray[3],
-      pagesContents.instructionsArray[3],
-      pagesContents.subNotesArray[3],
-      pagesContents.footerButtonsArray[3],
-      pages.pageFive
-    );
-  },
-  pageFive() {
-    pages.template(
-      true,
-      true,
-      true,
-      true,
-      pagesContents.headingsArray[4],
-      pagesContents.bodyButtonsArray[4],
-      pagesContents.instructionsArray[4],
-      pagesContents.subNotesArray[4],
-      pagesContents.footerButtonsArray[4],
-      pages.pageSix
-    );
-  },
-  pageSix() {
-    pages.template(
-      true,
-      false,
-      true,
-      true,
-      pagesContents.headingsArray[5],
-      pagesContents.bodyButtonsArray[5],
-      pagesContents.instructionsArray[5],
-      pagesContents.subNotesArray[5],
-      pagesContents.footerButtonsArray[5]
-    );
-  },
-};
+function clearPage() {
+  document.body.innerHTML = '';
+}
 
-pages.pageOne();
-
+function renderPage() {
+  clearPage();
+  createAndAppendElement(
+    'h2',
+    document.body,
+    content.headingsArray[state.currentPage - 1]
+  );
+  createAndAppendElement(
+    'button',
+    document.body,
+    content.bodyButtonsArray[state.currentPage - 1]
+  );
+  createAndAppendElement(
+    'p',
+    document.body,
+    content.instructionsArray[state.currentPage - 1]
+  );
+  createAndAppendElement(
+    'p',
+    document.body,
+    content.subNotesArray[state.currentPage - 1]
+  );
+  createAndAppendElement(
+    'button',
+    document.body,
+    content.footerButtonsArray[state.currentPage - 1]
+  );
+}
+renderPage();
 /*
+DATA
+declare a state object and set it to 1
+create content object with arrays of different dynamic data such as heading names and button names
 
-HTML STRUCTURE
+Different functions
+function to create/append elements
 
-  <body>
-    <div id="content-container" class="container">
-      <h2 id="heading"></h2>
-      <button id="body-button"></button>
-      <p id="instructions"></p>
-      <button id="footer-button"></button>
-    </div>
-    <script defer src="./pseudo-code.js"></script>
-  </body>
+function to clear page
 
-  
+function to render the page (this is where the logic of whatever the state is will declare that index data from each content array)
 
-MIND READER
+function to increment state
 
-    FUNCTIONALITIES
+function to decrement state
 
-        Has 6 pages
+function to go home
 
-            Each page should be a unique function within an object
-
-        Each page shares a navigator at the bottom that allows the user to cycle through them
-
-        The navigator remembers which page you are on and displays it
-
-        Has the ability to go back home or one page up or one page down
-
-    HOW TO MAKE THESE FUNCTIONALITIES WORK
-    
-        Have a central storage for each page held within functions (probably an object)
-
-        the navigation functions will have 3 functionalities buttons forward backwards or home
-        
-            Forward function will take **what page were currently on** and increment it by 1
-
-            Backward function wil take **what page were currently on** and decrement it by 1 
-
-            Home function will set **the page were currently on** to 1
-        
-        Each page has a CTA either Go, next, reveal that are buttons that will call the forward function
-        
-        Each page besides the first will have a home button that will call teh home function
 */
